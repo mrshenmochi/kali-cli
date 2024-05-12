819f6e4bab56022181ccb58460feee2c4b5cc657
sercrac07 <sercassan@gmail.com>
2024-05-12 22:31:14 +0200

    Update coinp package

--- update.js
+++ update.js
@@ -1,20 +1,18 @@
 #! /usr/bin/env node
 
-import { outro } from 'coinp'
-import { loader } from 'coinp'
-import { intro } from 'coinp'
+import * as coinp from 'coinp'
 import { execSync } from 'node:child_process'
 
-intro('System update')
+coinp.intro('System update')
 const startTime = new Date()
 
 const currentUser = execSync('whoami')
   .toString()
   .replace(/(\r\n|\n|\r)/gm, '')
 
-if (currentUser !== 'root') outro('You must be sudo to continue')
+if (currentUser !== 'root') coinp.outro('You must be sudo to continue')
 else {
-  const updateLoader = loader('System update', 'System upgrade')
+  const updateLoader = coinp.loader('System update', 'System upgrade')
   updateLoader.start()
   execSync('apt-get update')
   updateLoader.next('System updated')
@@ -26,5 +24,5 @@ else {
   const diffTime = Math.abs(endTime - startTime)
   const diffSeconds = Math.floor(diffTime / 1000)
 
-  outro(`Ended update in ${diffSeconds} second${diffSeconds !== 1 ? 's' : ''}`)
+  coinp.outro(`Ended update in ${diffSeconds} second${diffSeconds !== 1 ? 's' : ''}`)
 }

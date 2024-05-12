60b19a977f94cf975a4524350f490afb00f57352
sercrac07 <sercassan@gmail.com>
2024-05-12 18:21:46 +0200

    Create update script

--- update.js
+++ update.js
@@ -1,10 +1,30 @@
 #! /usr/bin/env node
 
+import { outro } from 'coinp'
+import { loader } from 'coinp'
+import { intro } from 'coinp'
 import { execSync } from 'node:child_process'
 
-const currentUser = execSync('whoami').toString()
-const sudo = currentUser === 'root' ? '' : 'sudo'
+intro('System update')
+const startTime = new Date()
 
-console.time('Update duration')
-execSync(`${sudo} apt-get update && ${sudo} apt-get upgrade -y`)
-console.timeEnd('Update duration')
+const currentUser = execSync('whoami')
+  .toString()
+  .replace(/(\r\n|\n|\r)/gm, '')
+
+if (currentUser !== 'root') outro('You must be sudo to continue')
+else {
+  const updateLoader = loader('System update', 'System upgrade')
+  updateLoader.start()
+  execSync('apt-get update')
+  updateLoader.next('System updated')
+  execSync('apt-get upgrade -y')
+  updateLoader.end('System updated')
+
+  const endTime = new Date()
+
+  const diffTime = Math.abs(endTime - startTime)
+  const diffSeconds = Math.floor(diffTime / 1000)
+
+  outro(`Ended update in ${diffSeconds} second${diffSeconds !== 1 ? 's' : ''}`)
+}
